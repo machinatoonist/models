6e0624402d746ab12b444098403933b5424fa548
machinatoonist <machinatoonist@users.noreply.github.com>
2022-05-07 06:40:02 +0000

    deploy: 481e232346017908b6d29615f055028f12ff5173

---
+++ assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "An Example Markdown Post",
+        "excerpt":"Example Markdown Post Basic setup Jekyll requires blog post files to be named according to the following format: YEAR-MONTH-DAY-filename.md Where YEAR is a four-digit number, MONTH and DAY are both two-digit numbers, and filename is whatever file name you choose, to remind yourself what this post is about. .md is...","categories": ["markdown"],
+        "tags": [],
+        "url": "https://machinatoonist.github.io/models/markdown/2020/01/14/test-markdown-post.html"
+      },{
+        "title": "Fastpages Notebook Blog Post",
+        "excerpt":"About This notebook is a demonstration of some of capabilities of fastpages with notebooks. With fastpages you can save your jupyter notebooks into the _notebooks folder at the root of your repository, and they will be automatically be converted to Jekyll compliant blog posts! Front Matter The first cell in...","categories": ["jupyter"],
+        "tags": [],
+        "url": "https://machinatoonist.github.io/models/jupyter/2020/02/20/test.html"
+      }]
